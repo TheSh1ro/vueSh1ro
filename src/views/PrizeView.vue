eaf11fd1b97e0270c0f81ba79aadc873f99ab4a5
thesh1ro <gabrielmonteiro1111@gmail.com>
2023-09-22 13:18:53 -0300

    Estilizada a tabela principal

--- src/views/PrizeView.vue
+++ src/views/PrizeView.vue
@@ -2,89 +2,87 @@
 export default {
   data() {
     return {
-      data: {
-        currentElo: [
-          {
-            name: 'Ferro',
-            leagues: [{ name: 'I' }, { name: 'II' }, { name: 'III' }, { name: 'IV' }],
-            visible: true
-          },
-          {
-            name: 'Bronze',
-            leagues: [{ name: 'I' }, { name: 'II' }, { name: 'III' }, { name: 'IV' }],
-            visible: true
-          },
-          {
-            name: 'Prata',
-            leagues: [{ name: 'I' }, { name: 'II' }, { name: 'III' }, { name: 'IV' }],
-            visible: true
-          },
-          {
-            name: 'Ouro',
-            leagues: [{ name: 'I' }, { name: 'II' }, { name: 'III' }, { name: 'IV' }],
-            visible: true
-          },
-          {
-            name: 'Platina',
-            leagues: [{ name: 'I' }, { name: 'II' }, { name: 'III' }, { name: 'IV' }],
-            visible: true
-          },
-          {
-            name: 'Esmeralda',
-            leagues: [{ name: 'I' }, { name: 'II' }, { name: 'III' }, { name: 'IV' }],
-            visible: true
-          },
-          {
-            name: 'Diamante',
-            leagues: [{ name: 'I' }, { name: 'II' }, { name: 'III' }, { name: 'IV' }],
-            visible: true
-          },
-          { name: 'Mestre', visible: true },
-          { name: 'Grão Mestre', leagues: null, visible: true },
-          { name: 'Desafiante', leagues: null, visible: true }
-        ],
-        targetElo: [
-          {
-            name: 'Ferro',
-            leagues: [{ name: 'I' }, { name: 'II' }, { name: 'III' }, { name: 'IV' }],
-            visible: true
-          },
-          {
-            name: 'Bronze',
-            leagues: [{ name: 'I' }, { name: 'II' }, { name: 'III' }, { name: 'IV' }],
-            visible: true
-          },
-          {
-            name: 'Prata',
-            leagues: [{ name: 'I' }, { name: 'II' }, { name: 'III' }, { name: 'IV' }],
-            visible: true
-          },
-          {
-            name: 'Ouro',
-            leagues: [{ name: 'I' }, { name: 'II' }, { name: 'III' }, { name: 'IV' }],
-            visible: true
-          },
-          {
-            name: 'Platina',
-            leagues: [{ name: 'I' }, { name: 'II' }, { name: 'III' }, { name: 'IV' }],
-            visible: true
-          },
-          {
-            name: 'Esmeralda',
-            leagues: [{ name: 'I' }, { name: 'II' }, { name: 'III' }, { name: 'IV' }],
-            visible: true
-          },
-          {
-            name: 'Diamante',
-            leagues: [{ name: 'I' }, { name: 'II' }, { name: 'III' }, { name: 'IV' }],
-            visible: true
-          },
-          { name: 'Mestre', visible: true },
-          { name: 'Grão Mestre', leagues: null, visible: true },
-          { name: 'Desafiante', leagues: null, visible: true }
-        ],
-        selectedElo: { current: { name: null, league: null }, target: { name: null, league: null } }
-      }
+      currentElo: [
+        {
+          name: 'Ferro',
+          leagues: [{ name: 'I' }, { name: 'II' }, { name: 'III' }, { name: 'IV' }],
+          visible: true
+        },
+        {
+          name: 'Bronze',
+          leagues: [{ name: 'I' }, { name: 'II' }, { name: 'III' }, { name: 'IV' }],
+          visible: true
+        },
+        {
+          name: 'Prata',
+          leagues: [{ name: 'I' }, { name: 'II' }, { name: 'III' }, { name: 'IV' }],
+          visible: true
+        },
+        {
+          name: 'Ouro',
+          leagues: [{ name: 'I' }, { name: 'II' }, { name: 'III' }, { name: 'IV' }],
+          visible: true
+        },
+        {
+          name: 'Platina',
+          leagues: [{ name: 'I' }, { name: 'II' }, { name: 'III' }, { name: 'IV' }],
+          visible: true
+        },
+        {
+          name: 'Esmeralda',
+          leagues: [{ name: 'I' }, { name: 'II' }, { name: 'III' }, { name: 'IV' }],
+          visible: true
+        },
+        {
+          name: 'Diamante',
+          leagues: [{ name: 'I' }, { name: 'II' }, { name: 'III' }, { name: 'IV' }],
+          visible: true
+        },
+        { name: 'Mestre', visible: true },
+        { name: 'Grão Mestre', leagues: null, visible: true },
+        { name: 'Desafiante', leagues: null, visible: true }
+      ],
+      targetElo: [
+        {
+          name: 'Ferro',
+          leagues: [{ name: 'I' }, { name: 'II' }, { name: 'III' }, { name: 'IV' }],
+          visible: true
+        },
+        {
+          name: 'Bronze',
+          leagues: [{ name: 'I' }, { name: 'II' }, { name: 'III' }, { name: 'IV' }],
+          visible: true
+        },
+        {
+          name: 'Prata',
+          leagues: [{ name: 'I' }, { name: 'II' }, { name: 'III' }, { name: 'IV' }],
+          visible: true
+        },
+        {
+          name: 'Ouro',
+          leagues: [{ name: 'I' }, { name: 'II' }, { name: 'III' }, { name: 'IV' }],
+          visible: true
+        },
+        {
+          name: 'Platina',
+          leagues: [{ name: 'I' }, { name: 'II' }, { name: 'III' }, { name: 'IV' }],
+          visible: true
+        },
+        {
+          name: 'Esmeralda',
+          leagues: [{ name: 'I' }, { name: 'II' }, { name: 'III' }, { name: 'IV' }],
+          visible: true
+        },
+        {
+          name: 'Diamante',
+          leagues: [{ name: 'I' }, { name: 'II' }, { name: 'III' }, { name: 'IV' }],
+          visible: true
+        },
+        { name: 'Mestre', visible: true },
+        { name: 'Grão Mestre', leagues: null, visible: true },
+        { name: 'Desafiante', leagues: null, visible: true }
+      ],
+      selectedElo: { current: { name: null, league: null }, target: { name: null, league: null } }
     }
   }
 }
@@ -93,12 +91,19 @@ export default {
   <main id="main">
     <div id="content">
       <div class="rankBox">
-        <div class="rankBox-column">
-          <template v-for="elo in this.currentElo">
-            <p>{{ elo.name }}</p>
+        <div class="rank-column">
+          <template v-for="(elo, index) in this.currentElo" :key="index">
+            <div class="rank-row">
+              <span class="row-elo" v-if="0">{{ elo.name }}</span>
+              <div class="row-league" v-if="1">
+                <span class="league-item">I</span>
+                <span class="league-item">II</span>
+                <span class="league-item">III</span>
+                <span class="league-item">IV</span>
+              </div>
+            </div>
           </template>
         </div>
-        <div class="rankBox-column">a</div>
       </div>
 
       <div class="priceBox">
@@ -126,9 +131,40 @@ export default {
   margin: 40px;
 
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(2, 1fr);
 }
 
-.rank-container {
+.rankBox {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  border: 1px solid white;
+}
+
+.rank-column {
+  display: grid;
+  grid-template-rows: repeat(10, 1fr);
+}
+
+.rank-row {
+  display: flex;
+}
+
+.row-elo {
+  flex-grow: 1;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.row-elo:hover {
+  background-color: rgba(250, 250, 0, 0.3);
+}
+
+.row-league {
+  flex: 1;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  background-color: red;
 }
 </style>
