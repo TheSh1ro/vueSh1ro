8a970870ba122d00fd58e83b0cb72a0b13d146db
thesh1ro <gabrielmonteiro1111@gmail.com>
2023-10-03 00:59:45 -0300

    Criado dropdown

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,7 +1,36 @@
+<template>
+  <header id="header">
+    <RouterLink to="/" class="item">sh1roJobs</RouterLink>
+    <div class="item">
+      <div class="item-user" @click="redirectToAccount">
+        <span>{{ username }}</span>
+        <img v-if="isUserLoggedIn" class="user-image" src="../assets/user.webp" alt="" />
+      </div>
+    </div>
+    <div v-if="showDropdown" class="dropdown">
+      <header>
+        <img
+          src="https://t.ctcdn.com.br/JlHwiRHyv0mTD7GfRkIlgO6eQX8=/640x360/smart/i257652.jpeg"
+          alt=""
+        />
+      </header>
+      <body>
+        <span>Desconectar</span>
+      </body>
+    </div>
+  </header>
+</template>
+
 <script>
 import { useAuthStore } from '../stores/store.js'
 
 export default {
+  data() {
+    return {
+      showDropdown: false
+    }
+  },
+
   computed: {
     username() {
       const authStore = useAuthStore()
@@ -12,19 +41,19 @@ export default {
       const authStore = useAuthStore()
       return !!authStore.user // Verifica se o usuário está logado
     }
+  },
+
+  methods: {
+    redirectToAccount() {
+      if (this.isUserLoggedIn) {
+        this.showDropdown = !this.showDropdown
+      } else {
+        this.$router.push('/account')
+      }
+    }
   }
 }
 </script>
-
-<template>
-  <header id="header">
-    <RouterLink to="/" class="item">sh1roJobs</RouterLink>
-    <RouterLink to="/account" class="item">
-      <span>{{ username }}</span>
-      <img v-if="isUserLoggedIn" src="../assets/user.webp" alt="" />
-    </RouterLink>
-  </header>
-</template>
 
 <style scoped>
 #header {
@@ -37,6 +66,7 @@ export default {
   align-items: center;
 
   transition: background-color 0.2s;
+  position: relative;
 }
 
 #header:hover {
@@ -45,22 +75,67 @@ export default {
 
 .item {
   display: flex;
-  align-items: center;
+  flex-direction: row;
   justify-content: center;
-  cursor: pointer;
-  padding-inline: 10px;
-  transition:
-    color 0.5s,
-    text-shadow 0.5s;
-  height: 100%;
+  align-items: center;
+
   gap: 3px;
+  height: 100%;
+  padding-inline: 10px;
+
+  cursor: pointer;
+  transition: color 0.2s;
 }
 
 .item:hover {
-  text-shadow: 0px 0px 5px white;
+  color: lightcyan;
 }
 
-img {
+.item-user {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-right: 70px;
+  gap: 3px;
+}
+
+.dropdown {
+  position: absolute;
+  right: 0px;
+  top: 150%;
+  width: 140px;
+  margin-right: 60px;
+
+  display: grid;
+  flex-direction: column;
+
+  background-color: rgb(8, 58, 91);
+  font-size: 1.1rem;
+}
+.dropdown > header > img {
+  width: 140px;
+  height: 40px;
+  display: block;
+  margin: 0 auto;
+  object-fit: cover;
+}
+.dropdown > body {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  cursor: pointer;
+}
+
+.dropdown > body > span {
+  padding: 2px;
+}
+.dropdown > body > span:hover {
+  display: block;
+  background-color: rgb(8, 98, 121);
+}
+
+.user-image {
   height: 4vh;
   border: 1px solid white;
   border-radius: 50px;
