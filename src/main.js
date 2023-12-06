988967848df6739f236cce43909fe80a5e1d5464
thesh1ro <gabrielmonteiro1111@gmail.com>
2023-12-05 22:42:20 -0300

    Remoção de importação bugada

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 // main.js
 
 import './assets/main.css'
-import VueScrollTo from 'vue-scrollto'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
@@ -9,11 +8,6 @@ import router from './router'
 
 const app = createApp(App)
 
-// Use VueScrollTo globalmente
-app.use(VueScrollTo, {
-  duration: 500, // Duração padrão do scroll (opcional)
-  easing: 'ease' // Tipo de easing (opcional)
-})
 
 // Adicione um método global para fazer scroll para o topo
 app.config.globalProperties.$scrollToTop = () => {
